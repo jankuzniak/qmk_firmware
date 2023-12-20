2e0493d25c5a95aca99225c3414da7a8533b411d
jan.kuzniak <jan.kuzniak@gmail.com>
2023-12-20 10:56:18 +0100

    typo

--- keyboards/sofle/keymaps/ysaren/combos.c
+++ keyboards/sofle/keymaps/ysaren/combos.c
@@ -1,6 +1,6 @@
 #include "process_combo.h"
 
-const uint16_t PROGMEM qw_esc[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM qw_gui[] = {KC_Q, KC_W, COMBO_END};
 combo_t key_combos[] = {
-    COMBO(qw_esc, KC_LGUI),
+    COMBO(qw_gui, KC_LGUI),
 };
