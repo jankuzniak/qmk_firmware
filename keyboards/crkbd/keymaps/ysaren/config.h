638160b96e5b904cd804b127e2108fc07a7e6e29
jan.kuzniak <jan.kuzniak@gmail.com>
2024-04-09 23:21:35 +0200

    layers moved, crkbd config adjusted

--- keyboards/crkbd/keymaps/ysaren/config.h
+++ keyboards/crkbd/keymaps/ysaren/config.h
@@ -62,5 +62,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #ifdef VIA_ENABLE
-    #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+    #define DYNAMIC_KEYMAP_LAYER_COUNT 11
 #endif
