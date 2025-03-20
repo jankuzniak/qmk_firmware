243c7e0c360d41d495dc5bb0a126233a1b44c7cd
jan.kuzniak <jan.kuzniak@gmail.com>
2025-03-20 10:16:23 +0100

    sofle, corne: capslock added

--- keyboards/crkbd/keymaps/ysaren/layout.c
+++ keyboards/crkbd/keymaps/ysaren/layout.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_SWITCH] = LAYOUT(
     _______,  KC_F1,          KC_F2,    KC_F3,       KC_F4,    KC_F5,           KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,   KC_F11,
     KC_LSFT,  TG(L_GAME_HW),  XXXXXXX,  XXXXXXX,     XXXXXXX,  XXXXXXX,         XXXXXXX,   OS_CTL,    OS_SFT,    OS_LALT,  OS_GUI,    KC_F12,
-    _______,  KC_PSCR,        KC_SCRL,  G(KC_PAUS),  KC_NLCK,  XXXXXXX,         F_ZONE_1,  F_ZONE_2,  F_ZONE_3,  KC_APP,   XXXXXXX,  XXXXXXX,
+    _______,  KC_PSCR,        KC_SCRL,  G(KC_PAUS),  KC_NLCK,  KC_CAPS,         F_ZONE_1,  F_ZONE_2,  F_ZONE_3,  KC_APP,   XXXXXXX,  XXXXXXX,
                                         _______,     _______,  _______,         _______,   KC_BSPC,   _______
 ),\
 
--- keyboards/sofle/keymaps/ysaren/layout.c
+++ keyboards/sofle/keymaps/ysaren/layout.c
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,  XXXXXXX,        XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,                          XXXXXXX,   XXXXXXX,    XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,
     _______,  KC_F1,          KC_F2,    KC_F3,      KC_F4,    KC_F5,                            KC_F6,     KC_F7,      KC_F8,     KC_F9,    KC_F10,   KC_F11,
     KC_LSFT,  TG(L_GAME_HW),  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,                          XXXXXXX,   OS_CTL,     OS_SFT,    OS_LALT,  OS_GUI,   KC_F12,
-    _______,  KC_PSCR,        KC_SCRL,  G(KC_PAUS), KC_NLCK,  XXXXXXX,  _______,      _______,  F_ZONE_1,  F_ZONE_2,   F_ZONE_3,  KC_APP,   XXXXXXX,  XXXXXXX,
+    _______,  KC_PSCR,        KC_SCRL,  G(KC_PAUS), KC_NLCK,  KC_CAPS,  _______,      _______,  F_ZONE_1,  F_ZONE_2,   F_ZONE_3,  KC_APP,   XXXXXXX,  XXXXXXX,
                               XXXXXXX,  XXXXXXX,    _______,  _______,  _______,      _______,  KC_BSPC,   _______,    XXXXXXX,   XXXXXXX
 ),
 
