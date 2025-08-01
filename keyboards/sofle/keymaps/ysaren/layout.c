9b0efacddcdb686a775e7ce39d83db53d9765b33
jan.kuzniak <jan.kuzniak@gmail.com>
2025-08-01 14:04:40 +0200

    sofle: added tab to game layer

--- keyboards/sofle/keymaps/ysaren/layout.c
+++ keyboards/sofle/keymaps/ysaren/layout.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer this one obscure game that has some odd controlls
 [L_GAME_HW] = LAYOUT(
    KC_ESC,   KC_MS_BTN1, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-   XXXXXXX,  KC_Q,       KC_W,     XXXXXXX,  XXXXXXX,  XXXXXXX,                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+   KC_TAB,   KC_Q,       KC_W,     XXXXXXX,  XXXXXXX,  XXXXXXX,                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
    KC_LSFT,  KC_1,       KC_2,     KC_3,     KC_4,     KC_5,                             XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
    XXXXXXX,  KC_A,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                          XXXXXXX,  XXXXXXX,  _______,  KC_Q,     _______,      _______,  _______,  _______,  XXXXXXX,  XXXXXXX
